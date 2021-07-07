ab6d266ae577562d1d3fad294e03b86db885bb3b
G Hemachandran <chandranchan321@gmail.com>
2021-07-07 19:29:39 +0530

    Fix: Moved the timerStore's set() to TypeProgress' onMount()

--- src/stores/utils/properties.js
+++ src/stores/utils/properties.js
@@ -1,5 +1,3 @@
-import {timer} from '../timerStore';
-
 const persistentProperties = [
     ['timer_duration', 60]
   ];
@@ -9,7 +7,6 @@ let properties = new Map(persistentProperties);
 export const updateTimerDuration = value => {
   if (![30, 60, 90, 120].includes(value)) return;
   properties.set('timer_duration', value);
-  timer.set(value);
 }
 
 export const getTimerDuration = () => properties.get('timer_duration');
