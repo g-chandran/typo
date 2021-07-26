01244adc03cc69ff85c79eac704a72110a1eef58
g-chandran <chandranchan321@gmail.com>
2021-07-26 09:25:03 +0530

    Added boolean return values for Property setters

--- src/stores/utils/properties.js
+++ src/stores/utils/properties.js
@@ -9,17 +9,24 @@ const persistentProperties = [
 
 let properties = new Map(persistentProperties);
 
+const UNITS = new Set([30, 60, 90]);
+
 export const updateTimerDuration = value => {
-  if (![30, 60, 90, 120].includes(value)) return;
-  properties.set('timer_duration', value);
+  if (UNITS.has(value)) {
+    properties.set('timer_duration', value);
+    return true;
+  } 
+  return false;
 }
 
 export const getTimerDuration = () => properties.get('timer_duration');
 
 export const setTheme = (theme) => {
-  if ([THEMES.LIGHT, THEMES.DARK].includes(theme)) {
+  if (Object.values(THEMES).includes(theme)) {
     properties.set('theme', theme);
+    return true;
   }
+  return false;
 }
 
 export const toggleTheme = () => {
@@ -30,15 +37,22 @@ export const toggleTheme = () => {
 export const getTheme = () => properties.get('theme');
 
 export const setPlaymode = (playmode) => {
-  if ([PLAYMODES.CLASSIC, PLAYMODES.REVERSE_CLASSIC].includes(playmode)) {
+  if (Object.values(PLAYMODES).includes(playmode)) {
     properties.set('playmode', playmode);
+    return true;
   }
+  return false;
 }
 
 export const getPlaymode = () => properties.get('playmode')
 
 export const setWordsLength = (length) => {
-  if ([30, 60, 90].includes(length)) properties.set('words_length', length);
+  if (UNITS.has(length)) {
+    properties.set('words_length', length);
+    return true;
+  }
+  return false;
+  // if ([30, 60, 90].includes(length)) properties.set('words_length', length);
 }
 
 export const getWordsLength = () => properties.get('words_length')
