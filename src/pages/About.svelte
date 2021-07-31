b9bc46af162f05073803a3337f77382dcb802ba2
g-chandran <chandranchan321@gmail.com>
2021-07-31 08:45:07 +0530

    Made basic layout to About Page

---
+++ src/pages/About.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { ABOUT_INFO } from "../stores/utils/constants";
+  import WordTile from "../components/WordTile.svelte";
+  import Button from "../components/Button.svelte";
+  const { CONTENT, TITLE } = ABOUT_INFO;
+
+  const wordList = CONTENT.split(":");
+
+  $: index = 0;
+
+  const updateWord = () => {
+    index += 1;
+  };
+</script>
+
+<div class="container">
+  <p>{TITLE}</p>
+  <WordTile word={wordList[index]} on:moveNext={updateWord} />
+  <Button
+    title="Go back"
+    name="Go to home"
+    onClickEventName="updateStage"
+    onClickEventProps={{ position: true }}
+    on:updateStage
+  />
+</div>
