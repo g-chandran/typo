0afaa2e2e8d19992d94cfa9ea2af43e39ef7760b
g-chandran <chandranchan321@gmail.com>
2021-08-11 08:43:48 +0530

    Removed esLint and added opionanated formatting with Prettier

--- src/stores/utils/constants.js
+++ src/stores/utils/constants.js
@@ -1,33 +1,46 @@
 export const STAGES = Object.freeze({
-	START: "start",
-	END: "end",
-	PROGRESS: "progress",
-	ABOUT: "about",
+  START: "start",
+  END: "end",
+  PROGRESS: "progress",
+  ABOUT: "about",
 });
 
 export const STATUS = Object.freeze({
-	UNWRITTEN: 'unWritten',
-	CORRECT: 'correct',
-	INCORRECT: 'incorrect'
+  UNWRITTEN: "unWritten",
+  CORRECT: "correct",
+  INCORRECT: "incorrect",
 });
 
 export const WORD_SET_THRESHOLD = 50;
 export const TIMER_DURATION = 60;
 
-export const COLORS = Object.freeze({ORANGE_COLOR: "#ff3e00", BLACK: "#000", WHITE: "#fff", GRAY: "#808080"})
-export const THEME_COLORS = Object.freeze({
-	DARK_1: "#28293d", DARK_2: "#555770", DARK_3: "#24292E",
-	LIGHT_1: "#fafafc", LIGHT_2: "#f2f2f5", LIGHT_3: "#ebebf0", 
+export const COLORS = Object.freeze({
+  ORANGE_COLOR: "#ff3e00",
+  BLACK: "#000",
+  WHITE: "#fff",
+  GRAY: "#808080",
 });
-export const THEMES = Object.freeze({DARK: "dark", LIGHT: "light"})
-export const BUTTON_COLORS = Object.freeze({ORANGE: "orange", THEME: "theme"})
+export const THEME_COLORS = Object.freeze({
+  DARK_1: "#28293d",
+  DARK_2: "#555770",
+  DARK_3: "#24292E",
+  LIGHT_1: "#fafafc",
+  LIGHT_2: "#f2f2f5",
+  LIGHT_3: "#ebebf0",
+});
+export const THEMES = Object.freeze({ DARK: "dark", LIGHT: "light" });
+export const BUTTON_COLORS = Object.freeze({
+  ORANGE: "orange",
+  THEME: "theme",
+});
 export const PLAYMODES = Object.freeze({
-	CLASSIC: "classic",
-	REVERSE_CLASSIC: "reverse_classic"
-})
+  CLASSIC: "classic",
+  REVERSE_CLASSIC: "reverse_classic",
+});
 
 export const ABOUT_INFO = Object.freeze({
-	TITLE: "You're here. Woo-hoo! Start typing to know about Typo.",
-	CONTENT: "Typo.:A Fast,:Powerful,:Playful app:to improve your typing skills.:" + 
-					"Playmodes,:Settings,:Themes:to make it more fun.:Now type it,:Typo it,:repeat it.:It keeps Evolving."
-})
+  TITLE: "You're here. Woo-hoo! Start typing to know about Typo.",
+  CONTENT:
+    "Typo.:A Fast,:Powerful,:Playful app:to improve your typing skills.:" +
+    "Playmodes,:Settings,:Themes:to make it more fun.:Now type it,:Typo it,:repeat it.:It keeps Evolving.",
+});
--- src/stores/utils/propertiesInterface.js
+++ src/stores/utils/propertiesInterface.js
@@ -1,29 +1,29 @@
-import { 
+import {
   setTheme,
   updateTimerDuration,
-  setPlaymode, 
+  setPlaymode,
   getPlaymode,
   setWordsLength,
-  getWordsLength
+  getWordsLength,
 } from "./properties";
 import { theme, timer, playmode, wordsLength } from "../masterStore";
 
 export const updateTheme = (requestedTheme) => {
   const result = setTheme(requestedTheme);
   if (result) theme.set(requestedTheme);
-}
+};
 
 export const updateTimer = (duration) => {
   const result = updateTimerDuration(duration);
   if (result) timer.set(duration);
-}
+};
 
 export const updatePlaymode = (requestedPlaymode) => {
   const result = setPlaymode(requestedPlaymode);
   if (result) playmode.set(getPlaymode());
-}
+};
 
 export const updateWordsLength = (length) => {
   const result = setWordsLength(length);
   if (result) wordsLength.set(getWordsLength());
-}
+};
